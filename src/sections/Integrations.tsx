8966b3a256e50917bcd14d4810171bba1563dde7
Mohandhass28 <mohandhass28@gmail.com>
2025-04-06 18:54:54 +0530

    remove unused variables

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import IntegrationsColumn from "@/components/IntegrationsColumn";
 import Tag from "@/components/Tag";
 import figmaIcon from "@/assets/images/figma-logo.svg";
